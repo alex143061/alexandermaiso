7e43b23314ad6596c27785b5f19ba6c7486d9255
Alex <alexandermaiso99@gmail.com>
2025-05-13 11:30:58 +0800

    Update something

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/alexander143061/', 
+//  // Add the base path for GitHub Pages
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alexander143061/', 
- // Add the base path for GitHub Pages
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
+    ? '/alexander143061/' // GitHub Pages
+    : '/',                // Vercel or local dev
 })
