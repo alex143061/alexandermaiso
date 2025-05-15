7a50d738b7389703423445cd0f20b2e88c4fe016
Alex <alexandermaiso99@gmail.com>
2025-05-15 15:01:25 +0800

    changed repo name

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
-    ? '/alex-devhub/' // GitHub Pages
+    ? '/alexandermaiso/' // GitHub Pages
     : '/',                // Vercel or local dev
 })
