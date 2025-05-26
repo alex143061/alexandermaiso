f01a73ea00ca24a7673bc2948cac734248e6870e
Alex <alexandermaiso99@gmail.com>
2025-05-26 22:00:07 +0800

    Added @media query to Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,8 +4,12 @@ import React from "react";
 const Dashboard = () => {
   return (
    <div className="w-full min-h-screen bg-primary text-white flex flex-col items-center justify-center -mt-24">
-      <h1 className="text-5xl font-bold mb-4">Welcome to your Dashboard!</h1>
-      <p className="text-lg">Work in Progress.</p>
+      <h1 className="text-2xl font-bold mb-4 text-center px-4 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+        Welcome to your Dashboard!
+      </h1>
+      <p className="text-lg text-center px-4 sm:text-xl md:text-2xl">
+        Work in Progress.
+      </p>
     </div>
   );
 };
