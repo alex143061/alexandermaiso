356e1d1b596b828fb88ad24d3a2ad1e8e4338be7
Alex <alexandermaiso99@gmail.com>
2025-05-27 01:30:08 +0800

    Added Session Storage

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/alexander143061/', 
-//  // Add the base path for GitHub Pages
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -17,4 +6,7 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
     ? '/alexandermaiso/' // GitHub Pages
     : '/',                // Vercel or local dev
+  build: {
+    sourcemap: false // ⛔ disables .map files in the build output
+  }
 })
